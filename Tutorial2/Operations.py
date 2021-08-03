586455f4716c6428ac5c64001617f70f15b8e30f
Himani Parida <76863075+HimaniParida@users.noreply.github.com>
2021-08-03 21:28:01 +0530

    Add files via upload

---
+++ Tutorial2/Operations.py
@@ -0,0 +1,27 @@
+#Addition
+a = 2 + 3
+print(a)
+
+#Subtraction 
+b = 9 - 7
+print(b)
+
+#Multiplication 
+c = 8 * 2
+print(c)
+
+#Division
+d = 5 / 2
+print(d)
+
+#Modulo
+e = 6 % 3
+print(e)
+
+#Roots
+f = 5 ** 2
+print(f)
+
+#Order of Operations
+g = 8 + 2 * 10 / 5 ** 4
+
---
+++ Tutorial2/Variables.py
@@ -0,0 +1,24 @@
+#Creating Variables
+x = 5
+y = "Himani"
+print(x)
+print(y)
+
+#Casting
+x = str(3)
+y = int(3)
+z = float(3)
+print(x)
+print(y)
+print(z)
+
+#Single or double quotes
+x = "Himani"
+x = 'Himani'
+print(x)
+
+#Case-sensitive
+a = 4
+A = "Himani"
+print(a)
+print(A)
