0d01a170c040faf6d96d8c9354091a67d3e9a746
Himani Parida <76863075+HimaniParida@users.noreply.github.com>
2021-08-04 22:02:22 +0530

    Add files via upload

---
+++ Tutorial3/Numbers.py
@@ -0,0 +1,16 @@
+#There are 3 numeric types in Python
+    #Int
+    #Float
+    #Complex
+
+x = 1           
+y = 23.4        
+z = 2j          
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+print(x)
+print(y)
+print(z)
