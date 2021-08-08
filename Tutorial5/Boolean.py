f1f52d2f6a31d7e77a9e40550a861ed66a07a0e5
Himani Parida <76863075+HimaniParida@users.noreply.github.com>
2021-08-08 11:15:58 +0530

    Add files via upload

---
+++ Tutorial5/Boolean.py
@@ -0,0 +1,16 @@
+print(10 > 9)
+print(10 == 9)
+print(10 >= 7)
+
+a = 3000
+b = 455
+if b > a :
+    print("b is greater than a")
+else :
+    print("a is greater than b")
+
+
+x = "Hello"
+y = 15
+print(bool(x))
+print(bool(y))
