18ce910849fdf57410f8df04913ccc95285f0490
Himani Parida <76863075+HimaniParida@users.noreply.github.com>
2021-08-10 08:37:37 +0530

    Add files via upload

---
+++ Tutorial7/Join_tuples.py
@@ -0,0 +1,12 @@
+#Joinig two tuples
+my_tuple1 = ("a", "b" , "c")
+my_tuple2 = (1, 2, 3)
+
+my_tuple3 = my_tuple1 + my_tuple2
+print(my_tuple3)
+
+#Multiply Two tuples
+fruits = ("apple", "banana", "cherry")
+my_tuple = fruits * 2
+
+print(my_tuple)
