e5f66f62a9c12644818a3f20778baade8cb52fc6
Himani Parida <76863075+HimaniParida@users.noreply.github.com>
2021-08-27 20:44:21 +0530

    Add files via upload

---
+++ Tutorial13/Funcions.py
@@ -0,0 +1,26 @@
+#Creating a function
+    #A function is a block of code which only runs when it is called.
+    #You can pass data, known as parameters, into a function.
+    #A function can return data as a result.
+def function_name():
+    print("Hello!!!")
+
+
+
+#Calling a function
+def function_name():
+    print("Hello!!! My name is Himani Parida")
+
+function_name();
+
+
+#Arguments - Arguments are specified after the function name, inside the parentheses. We can add as many arguments as we want, just separate them with a comma
+def my_function(fname):
+    print(fname + " Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
+
+
+
