ce5eeb05a6b46f466778e99863610313ece3a1c3
Himani Parida <76863075+HimaniParida@users.noreply.github.com>
2021-09-05 08:21:34 +0530

    Update Food_Bills.py

--- Assignements/Food_Bills.py
+++ Assignements/Food_Bills.py
@@ -1,6 +1,6 @@
 def create(order):
     file = open('./Assignements/sales.txt' , 'w')
-    file.write("Burger King\nPatia Bhubaneswar- 751023" + '\n' + '---------------------------------------'+'\n\n')
+    file.write("Biggies\nPatia Bhubaneswar- 751023" + '\n' + '---------------------------------------'+'\n\n')
     total = 0
     for item, price in order.items():
         file.write('- ' + item + ' ' + format(price, '.2f') + '\n')
@@ -8,12 +8,12 @@ def create(order):
     sgst = cgst = (total * 9)/100
     total += cgst + sgst
     
-    file. write('\n          S.G.S.T @9% = ' + format(sgst,'.2f')+ '\n')
-    file. write('          C.G.S.T @9% = ' + format(cgst,'.2f')+ '\n')
+    file. write('\n          S.G.S.T @7.5% = ' + format(sgst,'.2f')+ '\n')
+    file. write('          C.G.S.T @8% = ' + format(cgst,'.2f')+ '\n')
     file. write('             Subtotal = ' + format(total,'.2f')+ '\n\n\n')
     
     file.write('---------------------------------------' + '\n' + 'Thank you for coming !! ')
     file.close()
     
-my_dict={'Chicken Fiery Burger ': 200 , 'Med Fries            ': 89 , 'Veggie Strips        ': 49 , 'Pepsi Can            ': 60 , 'Grilled Wings        ': 350 ,'Blue Lagoon          ': 180}
+my_dict={'Chicken  Burger ': 200 , 'Chessy Fries            ': 90 , 'Veggie Rolls        ': 65, 'Cappucino           ': 85 , 'Grilled Sandwich       ': 350 ,'Blue Mohito          ': 180}
 create(my_dict)
