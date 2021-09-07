cea98941cf0e3dd5121959073db081895952cbb8
Himani Parida <76863075+HimaniParida@users.noreply.github.com>
2021-09-07 21:17:48 +0530

    Update Set_1.py

--- List_questions/Set_1.py
+++ List_questions/Set_1.py
@@ -44,3 +44,49 @@ for x in a:
         dup_items.add(x)
 
 print(dup_items)
+
+# 6. Write a Python program to check a list is empty or not
+l = []
+if not l:
+  print("List is empty")
+  
+
+# 7.Write a Python program to clone or copy a list
+list1 = [10, 52, 64, 27, 4]
+list2 = list(list1)
+print(list1)
+print(list2)
+
+
+# 8.Write a Python program to find the list of words that are longer than n from a given list of words.
+def long_words(n, str):
+    word_len = []
+    txt = str.split(" ")
+    for x in txt:
+        if len(x) > n:
+            word_len.append(x)
+    return word_len	
+print(long_words(3, "She turned her can't into can and dreams into plans"))
+
+
+#9. Write a Python function that takes two lists and returns True if they have at least one common member
+def com_data(list1, list2):
+     result = False
+     for x in list1:
+         for y in list2:
+             if x == y:
+                 result = True
+                 return result
+print(com_data([1,2,3,4,5], [5,6,7,8,9]))
+print(com_data([1,2,3,4,5], [6,7,8,9]))
+
+
+# 10.Write a Python program to get a list, sorted in increasing order by the last element in each tuple from a given list of non-empty tuples
+def last(n): return n[-1]
+
+def sort_list_last(tuples):
+  return sorted(tuples, key=last)
+
+print(sort_list_last([(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]))
+
+
