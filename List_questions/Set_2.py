d2372c35389ca8af0e85661d383f71187d45e7af
Himani Parida <76863075+HimaniParida@users.noreply.github.com>
2021-09-07 21:28:35 +0530

    Add files via upload

---
+++ List_questions/Set_2.py
@@ -0,0 +1,21 @@
+# 11.Write a Python program access the index of a list. 
+nums = [5, 15, 35, 8, 98]
+for num_index, num_val in enumerate(nums):
+    print(num_index, num_val)
+
+
+# 2.Write a Python program to find the index of an item in a specified list
+num =[10, 30, 4, -6]
+print(num.index(30))
+
+
+# 3.Write a Python program to append a list to the second list
+list1 = [1, 2, 3, 0]
+list2 = ['Red', 'Green', 'Black']
+sum_list = list1 + list2
+print(sum_list)
+
+
+
+
+	
