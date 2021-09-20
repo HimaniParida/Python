ca01bac1d59b09a0422d16d32cafa4b3c7e9e922
Himani Parida <76863075+HimaniParida@users.noreply.github.com>
2021-09-20 07:25:05 +0530

    Add files via upload

---
+++ Dictionaries_questions/Q11.py
@@ -0,0 +1,9 @@
+# 11.Write a Python program to sort a given dictionary by key
+color_dict = {'red':'#FF0000',
+          'green':'#008000',
+          'black':'#000000',
+          'white':'#FFFFFF'}
+
+for key in sorted(color_dict):
+    print("%s: %s" % (key, color_dict[key]))
+	
