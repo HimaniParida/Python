e18266d245fc5a983335707e68cef9da6478db24
Himani Parida <76863075+HimaniParida@users.noreply.github.com>
2021-09-22 22:53:46 +0530

    Add files via upload

---
+++ Sets_questions/Q1.py
@@ -0,0 +1,14 @@
+# 1.Write a Python program to create a set
+
+print("Create a new set:")
+x = set()
+print(x)
+print(type(x))
+print("\nCreate a non empty set:")
+n = set([0, 1, 2, 3, 4])
+print(n)
+print(type(n))
+print("\nUsing a literal:")
+a = {1,2,3,'foo','bar'}
+print(type(a))
+print(a)
---
+++ Sets_questions/Q2.py
@@ -0,0 +1,9 @@
+# 2. Write a Python program to iteration over sets
+
+num_set = set([0, 1, 2, 3, 4, 5])
+for n in num_set:
+  print(n, end=' ')
+print("\n\nCreating a set using string:")
+char_set = set("himaniparida")
+for val in char_set:
+    print(val, end=' ')
