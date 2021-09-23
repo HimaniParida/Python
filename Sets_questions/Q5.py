d20bed1036d9485d27d7f28dba8f7f37b2fde546
Himani Parida <76863075+HimaniParida@users.noreply.github.com>
2021-09-23 10:02:20 +0530

    Add files via upload

---
+++ Sets_questions/Q5.py
@@ -0,0 +1,17 @@
+# 5. Write a Python program to remove an item from a set if it is present in the set
+
+num_set = set([0, 1, 2, 3, 4, 5])
+print("Original set elements:")
+print(num_set)
+print("\nRemove 0 from the said set:")
+num_set.discard(4)
+print(num_set)
+print("\nRemove 5 from the said set:")
+num_set.discard(5)
+print(num_set)
+print("\nRemove 2 from the said set:")
+num_set.discard(5)
+print(num_set)
+print("\nRemove 7 from the said set:")
+num_set.discard(15)
+print(num_set)
