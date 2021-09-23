ff711b00059a7445a89e1924e18954a90f47db2b
Himani Parida <76863075+HimaniParida@users.noreply.github.com>
2021-09-23 10:01:59 +0530

    Add files via upload

---
+++ Sets_questions/Q4.py
@@ -0,0 +1,8 @@
+# 4. Write a Python program to remove item(s) from a given set. 
+
+num_set = set([0, 1, 3, 4, 5])
+print("Original set:")
+print(num_set)
+num_set.pop()
+print("\nAfter removing the first element from the said set:")
+print(num_set)
