ec84d2ee73d306faaf1a8c7d95d953ddd5675a80
Himani Parida <76863075+HimaniParida@users.noreply.github.com>
2021-09-25 19:49:54 +0530

    Add files via upload

---
+++ Tutorial17/Numpy.py
@@ -0,0 +1,7 @@
+#numpy - NumPy is a Python library used for working with arrays.It also has functions for working in domain of linear algebra, fourier transform, and matrices.
+
+#Creating Numpy 
+
+import numpy
+array = numpy.array([1,2,3,4,5,6,7,8])
+print(array)
