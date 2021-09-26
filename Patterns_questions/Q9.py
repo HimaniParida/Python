6d4d9b92879670586ca858932cdabadecabe8943
Himani Parida <76863075+HimaniParida@users.noreply.github.com>
2021-09-26 12:08:09 +0530

    Add files via upload

---
+++ Patterns_questions/Q9.py
@@ -0,0 +1,12 @@
+# reverse patterns of digit from 10
+start = 1
+stop = 2
+currentNumber = stop
+for row in range(2, 6):
+    for col in range(start, stop):
+        currentNumber -= 1
+        print(currentNumber, end=' ')
+    print("")
+    start = stop
+    stop += row
+    currentNumber = stop
