17ed2e7ca8f8693d61a39efd41f62a7affcdc16c
Himani Parida <76863075+HimaniParida@users.noreply.github.com>
2021-09-27 08:23:43 +0530

    Add files via upload

---
+++ Tutorial17/Array_shape.py
@@ -0,0 +1,8 @@
+# The shape of an array is the number of elements in each dimension
+
+#Getting the shape of an array 
+import numpy as np
+arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
+print(arr.shape)
+
+
