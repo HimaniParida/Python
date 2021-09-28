71510e8f4582339477779ae8aae45c5621a8350a
Himani Parida <76863075+HimaniParida@users.noreply.github.com>
2021-09-28 09:27:41 +0530

    Add files via upload

---
+++ Tutorial17/Array_sort.py
@@ -0,0 +1,22 @@
+# Sorting the arrays 
+#Sortimg ate arrays means putting the elements in tan arramnged manner
+
+import numpy as np
+arr = np.array([3, 2, 0, 1])
+print(np.sort(arr))
+
+
+import numpy as np
+arr = np.array(['banana', 'cherry', 'apple'])
+print(np.sort(arr))
+
+
+import numpy as np
+arr = np.array([True, False, True])
+print(np.sort(arr))
+
+
+#Sorting 2-D Arrays 
+import numpy as np
+arr = np.array([[3, 2, 4], [5, 0, 1]])
+print(np.sort(arr))
